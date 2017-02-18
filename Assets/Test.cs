cb8bfe9bc0319f1e24c6f1399dbcb059f0108153
Nriko Kobari <chomerico777pd@gmail.com>
2017-02-18 21:34:54 +0900

    Lesson4の課題です。 合格したら、発展課題に挑戦してみます。

---
+++ Assets/Test.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Test : MonoBehaviour {
+
+	void Start () {
+		
+		int[] array = { 11, 22, 33, 44, 55 };
+
+		Debug.Log (array [0]);
+		Debug.Log (array [1]);
+		Debug.Log (array [2]);
+		Debug.Log (array [3]);
+		Debug.Log (array [4]);
+
+		for (int i = array.Length - 1; 0 <= i; i-- ) {
+			Debug.Log (array[i]);
+			}
+
+	}
+
+	void Update () {
+
+	}
+}
